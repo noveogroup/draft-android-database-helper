f1e90e346dde702b82fdb5640b244e820d4ca174
Pavel Stepanov <stefan.nsk@gmail.com>
2013-03-21 22:08:35 +0700

    initialize project

---
+++ src/main/java/com/noveogroup/android/database/Main.java
@@ -0,0 +1,9 @@
+package com.noveogroup.android.database;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("draft-android-database-helper");
+    }
+
+}
